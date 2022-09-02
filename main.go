d153107245ac09d4f8a3a897c7c080afdd3b144f
Jason Underhill <jason@junderhill.com>
2022-09-02 12:32:05 +0100

    Add versioning info

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"sd-ingest/cmd"
 )
 
+var (
+	commit  string
+	builtAt string
+	version string
+)
+
 func main() {
+
+	fmt.Printf("\n\nSD Ingest Version %s\n", version)
+	fmt.Println("by Jason Underhill\n")
+	fmt.Println("https://github.com/junderhill/sd-ingest\n")
+	fmt.Printf("Built from %s at %s\n\n", commit, builtAt)
+
 	cmd.Execute()
 }
